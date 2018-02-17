f076eb033750555ad65363f8b9e309bc2764ff8a
Carl Edwin <carlinstr@gmail.com>
2018-02-17 16:04:29 -0200

    initial commit

---
+++ src/main/java/com/wordpress/carledwinj/AutenticacaoSpringBootJwtApplication.java
@@ -0,0 +1,12 @@
+package com.wordpress.carledwinj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticacaoSpringBootJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticacaoSpringBootJwtApplication.class, args);
+	}
+}
